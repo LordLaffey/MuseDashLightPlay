9d038950667f5114148fb063c41e28f094484153
LordLaffey <111254600+LordLaffey@users.noreply.github.com>
2022-10-13 17:57:04 +0800

    Add files via upload

--- Music.cpp
+++ Music.cpp
@@ -24,23 +24,27 @@ void Music::Load(vector<string>& list){
     ClearScreen();
     Print("Loading Music...\n", 20);
 
-    string path="/data/music/";
-    long   hFile   =   0;
+    string path=".\\data\\music\\";
+    long long hFile = 0;
     struct _finddata_t fileinfo;
-    vector<string> files;
     string p;
     if((hFile = _findfirst(p.assign(path).append("\\*.rbq").c_str(),&fileinfo)) !=  -1)
     {
         do
         {
             if(!(fileinfo.attrib &  _A_SUBDIR))
-                files.push_back(p.assign(path).append("\\").append(fileinfo.name) );
+            {
+                list.push_back(fileinfo.name);
+                vector<string> strlist;
+                cout<<"Loading "<<fileinfo.name<<endl;
+            }
         }while(_findnext(hFile, &fileinfo)  == 0);
         _findclose(hFile);
     }
     
     Sleep(OneSecond/5);
     cout << "Loaded!" ;
+    Sleep(OneSecond/5);
 
 }
 
